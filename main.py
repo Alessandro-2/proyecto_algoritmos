9d5f121294440c139e469ec7bb57724183fb694a
Maria Valentina Montilla Mata <mariavalentinamontillamata@MacBook-Air-de-Maria-9.local>
2025-08-08 13:25:15 -0400

    definí main

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import funciones_catalogo
 
 def main():
-
+    """
+    Muestra el menú principal del sistema MetroArt y gestiona la interacción con el usuario.
+    """
     print()
     while True:
-        print("Catálogo de Obras del Museo Metropolitano de Arte: \n \n1- Ver lista de obras por Departamento \n2- Ver lista de obras por Nacionalidad del autor \n3- Ver lista de obras por nombre del autor \n4- Salir del sistema")
+        print("Catálogo de Obras del Museo Metropolitano de Arte:\n")
+        print("1 - Ver lista de obras por Departamento")
+        print("2 - Ver lista de obras por Nacionalidad del autor")
+        print("3 - Ver lista de obras por nombre del autor")
+        print("4 - Salir del sistema")
 
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
             funciones_catalogo.ver_obras_por_departamento()
         elif opcion == "2":
-            pass
+            funciones_catalogo.ver_obras_por_nacionalidad()
         elif opcion == "3":
-            print("creacion de mi rama maria")
+            passs
         elif opcion == "4":
             print()
-            print("Saliendo del sistema...")    
+            print("Saliendo del sistema...")
             print()
             break
         else:
             print()
-            print("Opción invalida, por favor, intente de nuevo.")
+            print("Opción inválida, por favor, intente de nuevo.")
             print()
 
 main()
